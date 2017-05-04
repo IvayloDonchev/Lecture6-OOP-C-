62fba83fcdf9d3758aaa62c3c2a2abc97019d652
Ivaylo Donchev <idonchev@hotmail.com>
2017-05-04 19:37:33 +0300

    wdf

--- Shapes/Shapes/Program.cs
+++ Shapes/Shapes/Program.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Shapes
 {
     public abstract class Shape
     {
+        public int x, y; // координати на центъра на фигурата
         public abstract double Area { get; }
+        public Shape(int x=0, int y=0) { this.x = x; this.y = y; }
     }
 
     public class Rectangle : Shape
     {
         private int a, b; // дължини на страните
-        public Rectangle(int a, int b)
+        public Rectangle(int a, int b) : base(1,2)
         {
             this.a = a;
             this.b = b;
